e5db7b733f401ab7e8fc273121d4d4e4310ce2e7
XIANYUN QI <ucesxyq@ucl.ac.uk>
2018-05-09 01:57:36 -0400

    Question Setting App

--- ucesxyq/platforms/browser/www/js/uploadData.js
+++ ucesxyq/platforms/browser/www/js/uploadData.js
@@ -3,6 +3,9 @@ var client;
 
 function startDataUpload() {
 alert ("start data upload");
+// get the text boxes values by specific id //
+// and value each variable//
+// put them inside postString//
 var clientid = document.getElementById("clientid").value;
 var questionid = document.getElementById("questionid").value;
 var question = document.getElementById("question").value;
@@ -10,13 +13,13 @@ var answer1 = document.getElementById("answer1").value;
 var answer2 = document.getElementById("answer2").value;
 var answer3 = document.getElementById("answer3").value;
 var answer4 = document.getElementById("answer4").value;
-//alert(name + " "+ surname + " "+module);
+
 var latitude = document.getElementById("latitude").value;
 var longitude = document.getElementById("longitude").value;
 var postString = "clientid="+clientid +"&questionid="+questionid+"&question="+question+"&answer1="+answer1+"&answer2="+answer2+"&answer3="+answer3+"&answer4="+answer4; 
 postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
 
-// now get the radio button values
+// now get the radio button values//
 if (document.getElementById("answernumber1").checked) {
 var correct=document.getElementById("answernumber1").value;
 postString=postString+"&answernumber="+correct
@@ -37,6 +40,8 @@ postString=postString+"&answernumber="+correct
 
 processData(postString);}
 
+
+// upload data into the server//
 function processData(postString) {
 client = new XMLHttpRequest();
 client.open('POST','http://developer.cege.ucl.ac.uk:30272/uploadData',true);
@@ -45,8 +50,8 @@ client.onreadystatechange = dataUploaded;
 client.send(postString);
 
 }
-// create the code to wait for the response from the data server, and process the response once it is received
 
+// create the code to wait for the response from the data server, and process the response once it is received
 function dataUploaded() {
 // this function listens out for the server to say that the data is ready - i.e. has state 4
 if (client.readyState == 4) {
